5ec8ea76e5da8e57b1ecbd83d8d39e3fbc039e9f
mager <andrew.mager@gmail.com>
2025-11-28 07:12:24 -0600

    Redesign

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'mager.co';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'Chicago explorer coding recipes and building with AI. Adventures in food, tech, and learning.';
